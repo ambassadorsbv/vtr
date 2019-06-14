280928d988b3c816a454e7b3a5f0f1ab21a63695
JasonD <jason.dmail@gmail.com>
2019-06-14 18:04:47 +0200

    Added logo from own assets folder, cleaned up code a little. pep8-ified.

--- video/qc.py
+++ video/qc.py
@@ -4,7 +4,7 @@ import sys
 import subprocess as sp
 import json
 import os
-import PIL
+import time
 
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
@@ -17,28 +17,34 @@ pdfmetrics.registerFont(TTFont('GothamBold', 'Gotham-Bold.ttf'))
 pdfmetrics.registerFont(TTFont('GothamBook', 'Gotham-Book.ttf'))
 pdfmetrics.registerFont(TTFont('GothamMed', 'Gotham-Medium.ttf'))
 
-import reportlab.platypus
-import PIL
-
 
 if len(sys.argv) < 1:
     print("Missing input. Please input a file path.")
     exit(1)
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 inFile = sys.argv[1]
 inFileDir = os.path.dirname(inFile)
 inFileName = os.path.splitext(os.path.basename(inFile))[0]
 
 outThumb = os.path.join(
-              "{inDir}/{inFileName}_thumb.png".format(inDir=inFileDir,inFileName=inFileName)
+              "{inDir}/{inFileName}_thumb.png".format(
+                  inDir=inFileDir,
+                  inFileName=inFileName)
 )
 
 outLog = os.path.join(
-              "{inDir}/{inFileName}.log".format(inDir=inFileDir,inFileName=inFileName)
+              "{inDir}/{inFileName}.log".format(
+                  inDir=inFileDir,
+                  inFileName=inFileName)
 )
 
 outPDF = os.path.join(
-              "{inDir}/{inFileName}.pdf".format(inDir=inFileDir,inFileName=inFileName)
+              "{inDir}/{inFileName}.pdf".format(
+                  inDir=inFileDir,
+                  inFileName=inFileName)
 )
 
 
@@ -58,7 +64,7 @@ def probe_infile(inFile):
     ff_command.extend(ff_header)
     ff_command.append(inFile)
     ff_command.extend(ff_opts)
-    print("Probing input file...")
+    print("Probing input file {}".format(inFile))
     process = sp.run(ff_command, capture_output=True)
     probe_data = json.loads(process.stdout)
     print("Probe complete.")
@@ -77,8 +83,6 @@ def vidlen(probe_data):
 
 def fileinfo(probe_data):
     """Read the json log and output file information."""
-    fileSize = (os.path.getsize(inFile) / 1048576)
-    fileSize = "{0:.2f}".format(fileSize)
     for stream in probe_data["streams"]:
         streamcheck = stream.get("codec_type")
         if streamcheck == "video":
@@ -89,25 +93,39 @@ def fileinfo(probe_data):
             fps = stream.get("r_frame_rate")
             vcodec = stream.get("codec_name")
             vcodec_long = stream.get("codec_long_name")
-            date = stream.get("tags")["creation_time"]
+            vcodec_tag = stream.get("tags")["encoder"]
+            duration = float(stream.get("duration"))
+            duration = "{0:.2f}".format(duration)
+            bitrate = stream.get("bit_rate")
+            colorspace = stream.get("color_space")
         if streamcheck == "audio":
             achannels = stream.get("channels")
             acodec = stream.get("codec_name")
             acodec_long = stream.get("codec_long_name")
             samplerate = stream.get("sample_rate")
-    fileinfo_out = {"FileSize" : fileSize,
-                    "Width" : width, 
-                    "Height" : height, 
-                    "Ratio" : ratio, 
-                    "Order" : order, 
-                    "FPS" : fps, 
-                    "VideoCodec" : vcodec,
-                    "VideoCodecLong" : vcodec_long,
-                    "AudioCodec" : acodec,
-                    "AudioCodecLong" : acodec_long,
-                    "AudioChannels" : achannels,
-                    "SampleRate" : samplerate,
-                    "Date" : date}
+    fileSize = (os.path.getsize(inFile) / 1048576)
+    fileSize = "{0:.2f}".format(fileSize)
+    date = time.gmtime(os.path.getctime(inFile))
+    date = time.strftime("%Y-%m-%d at %H:%M", date)
+    bitrate = int(bitrate) / 1024 / 1024
+    bitrate = "{0:.2f}".format(bitrate)
+    fileinfo_out = {"FileSize": fileSize,
+                    "Width": width,
+                    "Height": height,
+                    "Ratio": ratio,
+                    "Order": order,
+                    "FPS": fps,
+                    "VideoCodec": vcodec,
+                    "VideoCodecLong": vcodec_long,
+                    "VideoCodecTag": vcodec_tag,
+                    "Duration": duration,
+                    "Bitrate": bitrate,
+                    "Colorspace": colorspace,
+                    "AudioCodec": acodec,
+                    "AudioCodecLong": acodec_long,
+                    "AudioChannels": achannels,
+                    "SampleRate": samplerate,
+                    "Date": date}
     return fileinfo_out
 # End fileinfo.
 
@@ -116,7 +134,9 @@ def loudness(inFile, probe_data):
     """Probe the input file and read the audio stream loudness."""
     for stream in probe_data["streams"]:
         if stream.get("codec_type") == "audio":
-            ff_opts = "amovie='{inFile}',ebur128=metadata=1".format(inFile=inFile)
+            ff_opts = "amovie='{inFile}',ebur128=metadata=1".format(
+                inFile=inFile
+            )
             ff_command = ["/usr/bin/env", "ffprobe",
                           "-f", "lavfi", ff_opts,
                           "-show_frames", "-of", "json", "-v", "quiet"
@@ -140,9 +160,9 @@ def thumbmaker(inFile, inFile_dur):
     """Create the thumbnail output."""
     thumb_ss = int(float(inFile_dur) * 0.1)
     thumb_tc = int(float(inFile_dur) * 0.5 - thumb_ss)
-    ff_opts = "select=not(mod(t\\,{timebase})),scale=300:-2,tile=3x1".format(
+    ff_opts = "select=not(mod(t\\,{timebase})),scale=-2:180,tile=3x1".format(
         timebase=thumb_tc
-        )
+    )
     ff_thumb = [
         '/usr/bin/env', 'ffmpeg', '-ss', str(thumb_ss),
         '-hide_banner', '-loglevel', 'error',
@@ -154,7 +174,7 @@ def thumbmaker(inFile, inFile_dur):
     print("Creating thumbnail file...")
     sp.run(ff_thumb)
     print("Thumbnail created.")
-    ff_thumbProbe = [ 
+    ff_thumbProbe = [
         '/usr/bin/env', 'ffprobe', outThumb,
         '-show_streams', '-of', 'json', '-v', 'quiet'
     ]
@@ -169,55 +189,52 @@ def thumbmaker(inFile, inFile_dur):
 
 def pdfmaker(probe_data, loudness):
     """Create a pdf report from our test data."""
-    ## Set canvas parameters.
+    # Set canvas parameters.
     x = 30
     y = 800
     c = canvas.Canvas(outPDF, pagesize=A4, bottomup=1, pageCompression=0)
     cWidth, cHeight = A4
     c.setAuthor("Ambassadors VFX")
     c.setTitle(inFileName)
+    # End canvas parameters.
 
-#    def centeredString(string, font, size):
-#        width = pdfmetrics.stringWidth(string, font, size)
-#        width += (len(string) - 1)
-#        centeredString = ((cWidth/2)- (width/2))
-#        return centeredString
-    ## End canvas parameters.
-
-    ## Set File Info
+    # Set File Info
     fileSize = str(fileinfo["FileSize"])
     fileWidth = str(fileinfo["Width"])
     fileHeight = str(fileinfo["Height"])
     fileRatio = str(fileinfo["Ratio"])
+    fileDur = str(fileinfo["Duration"])
+    fileBitrate = str(fileinfo["Bitrate"])
+    fileColor = str(fileinfo["Colorspace"])
     fileOrder = str(fileinfo["Order"])
     fileFPS = str(fileinfo["FPS"])
     fileVCodec = str(fileinfo["VideoCodec"])
-    fileVCodecLong = str(fileinfo["VideoCodecLong"])
+    fileVCodecLong = str(fileinfo["VideoCodecTag"])
     fileACodec = str(fileinfo["AudioCodec"])
     fileACodecLong = str(fileinfo["AudioCodecLong"])
     fileAChannels = str(fileinfo["AudioChannels"])
     fileSampleRate = str(fileinfo["SampleRate"])
     fileLoudness = str(loudness[0])
     fileR128 = str(loudness[1])
-    fileDate = str(fileinfo["Date"][:10])
+    fileDate = str(fileinfo["Date"])
 
-    
-    ## Draw a colored box to set background color.
+    # Draw a colored box to set background color.
     c.setFillColor(HexColor("#e7e5dd"))
-    c.rect(-1*cm,-5,25*cm,30*cm, fill=1)
-    ## End background color.
-    ## Reset the color to Ambassadors Green for writing text.
+    c.rect(-1*cm, -5, 25*cm, 30*cm, fill=1)
+    # Reset the color to Ambassadors Green for writing text.
     c.setFillColor(HexColor("#3e3905"))
+    # End background color.
 
-    ## Import the logo image. 
-    c.drawImage("/AMSHARED/AMBASSADORS/ARTWORK/AMBASSADORS_BRANDING_2016/LOGO_DARK_GREEN/Ambassadors_LogoName_Banner_DRK_RGB_1224px.png",
+    # Import the logo image.
+    c.drawImage("{}/qc/assets/amb_logo.png".format(__location__),
                 30, cHeight-100,
                 width=cWidth/2,
                 height=(cWidth/2)/4,
                 mask='auto'
     )
-    ## End logo image.
-    ## Print the file info.
+    # End logo image.
+
+    # Print the file info.
     y_fileDetails = y-130
 
     c.setFont("GothamBold", 22)
@@ -236,54 +253,61 @@ def pdfmaker(probe_data, loudness):
     c.drawString(x+10, y_fileDetails-59, "File Size:")
     c.drawString(x+130, y_fileDetails-59, "{val} MB".format(
                  val=fileSize))
-    ## End file info.
-    ## Show the thumbnail image.
+    # End file info.
+
+    # Show the thumbnail image.
     thumbWidth = thumbSizes[0]
     thumbHeight = thumbSizes[1]
     y_thumb = y_fileDetails-180
-    c.drawImage(outThumb, 
-                30, y_thumb, 
+    c.drawImage(outThumb,
+                30, y_thumb,
                 width=thumbWidth / 2,
                 height=thumbHeight / 2,
                 mask='auto'
     )
-#    if os.path.isfile(outThumb):
-#        os.remove(outThumb)
-    ## End thumbnail image.
+    if os.path.isfile(outThumb):
+        os.remove(outThumb)
+    # End thumbnail image.
 
-    ## Print the technical video details. 
+    # Print the technical video details.
     y_vidDetails = y_thumb-50
 
     c.setFont("GothamBold", 22)
-    c.drawString(x, y_vidDetails, "Video specifications")    
+    c.drawString(x, y_vidDetails, "Video specifications")
 
     c.setFont("GothamMed", 10)
-    c.drawString(x+10, y_vidDetails-20, "Resolution:") 
+    c.drawString(x+10, y_vidDetails-20, "Resolution:")
     c.drawString(x+130, y_vidDetails-20, "{width}x{height}".format(
                  width=fileWidth,
                  height=fileHeight))
     c.drawString(x+10, y_vidDetails-33, "Aspect Ratio:")
     c.drawString(x+130, y_vidDetails-33, "{val}".format(
                  val=fileRatio))
-    c.drawString(x+10, y_vidDetails-46, "Framerate:")
-    c.drawString(x+130, y_vidDetails-46, "{val}".format(
-                 val=fileFPS))
-    c.drawString(x+10, y_vidDetails-59, "Video Codec:")
+    c.drawString(x+10, y_vidDetails-46, "Duration:")
+    c.drawString(x+130, y_vidDetails-46, "{val} seconds".format(
+                val=fileDur))
+    c.drawString(x+10, y_vidDetails-59, "Framerate:")
     c.drawString(x+130, y_vidDetails-59, "{val}".format(
-                 val=fileVCodec))
-    c.drawString(x+10, y_vidDetails-72, "Field Order:")
+                 val=fileFPS))
+    c.drawString(x+10, y_vidDetails-72, "Video Codec:")
     c.drawString(x+130, y_vidDetails-72, "{val}".format(
+                 val=fileVCodecLong))
+    c.drawString(x+10, y_vidDetails-85, "Field Order:")
+    c.drawString(x+130, y_vidDetails-85, "{val}".format(
                  val=fileOrder))
-    ## End video details.
+    c.drawString(x+10, y_vidDetails-98, "Bitrate:")
+    c.drawString(x+130, y_vidDetails-98, "{val} Mbit/s".format(
+                 val=fileBitrate))
+    # End video details.
 
-    ## Print the technical video details. 
-    y_audDetails = y_vidDetails-120
+    # Print the technical video details.
+    y_audDetails = y_vidDetails-135
 
     c.setFont("GothamBold", 22)
-    c.drawString(x, y_audDetails, "Audio specifications")    
+    c.drawString(x, y_audDetails, "Audio specifications")
 
     c.setFont("GothamMed", 10)
-    c.drawString(x+10, y_audDetails-20, "Audio Channels:") 
+    c.drawString(x+10, y_audDetails-20, "Audio Channels:")
     c.drawString(x+130, y_audDetails-20, "{val}".format(
                  val=fileAChannels))
     c.drawString(x+10, y_audDetails-33, "Audio Codec:")
@@ -298,34 +322,27 @@ def pdfmaker(probe_data, loudness):
     c.drawString(x+10, y_audDetails-72, "R128 compliant:")
     c.drawString(x+130, y_audDetails-72, "{val}".format(
                  val=fileR128))
-    ## End audio details.
-    
-    ## Footer.
+    # End audio details.
+
+    # Footer.
     c.setFont("GothamMed", 6)
     footerString = "Ambassadors B.V. - https://www.ambassadors.com/ - AMS +31 20 435 7171 - NYC +1 347 308 5857"
     c.drawCentredString(cWidth/2, y-780, footerString)
-    #footer = c.beginText()
-    #footer.setFont("GothamMed", 6)
-    #footer.setTextOrigin(centeredString(footerString, "GothamMed", 6), y-780)
-    #footer.textLine(footerString)
-    #c.drawText(footer)
-    ## End footer.
+    # End footer.
+
     c.showPage()
     c.save()
+
     print("QC reported can be found at {}".format(outPDF))
-    
+    print(" ")
 # End pdfmaker.
+
 
 if __name__ == "__main__":
 
     probe_data = probe_infile(inFile)
     loudness = loudness(inFile, probe_data)
     fileinfo = fileinfo(probe_data)
-
-#    with open(outLog, "a") as logfile:
-#        for line in fileinfo:
-#            print(line, file=logfile)
-#        print(loudness, file=logfile)
 
     inFile_dur = vidlen(probe_data)
     thumbSizes = thumbmaker(inFile, inFile_dur)
