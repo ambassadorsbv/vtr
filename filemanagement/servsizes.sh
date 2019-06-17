57154684bc4f61a3c412b4495259a4937455720f
Jorn Bergmans <jorn.bergmans@ambassadors.studio>
2019-06-17 16:22:55 +0200

    Adding script that scrobbles server sizes each night.

---
+++ filemanagement/servsizes.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+IFS=$'\n'
+datetime=$(date +%Y%m%d-%H%M)
+
+# # # # # # # # # # # #
+# The Hammer Chapter  #
+# # # # # # # # # # # #
+
+du -kd1 /AMBASSADORS_SHARED | sort -nr | sed $'s/[[:blank:]]/,/;s/\/.*\///;s/[[:space:]]/-/g;s/\///g;/\,\./d' >> /Volumes/HEIMDALL/LIBRARY/studio/servsizes/all_servers/hammer/shared/shared_$datetime.csv
+#
+du -kd1 /HAMMER | sed $'s/[[:blank:]]/,/;s/\/.*\///;s/[[:space:]]/-/g;s/\///g;/\,\./d' | sort -nr >> /Volumes/HEIMDALL/LIBRARY/studio/servsizes/all_servers/hammer/hammer/hammer_$datetime.csv
+
+# # # # # # # # # # # #
+#    The Projects     #
+# # # # # # # # # # # #
+
+find /AMBASSADORS_SHARED/PROJECTS/ -type d -and -iname "*_p1??????" -and -not -ipath "*_p1??????/*_p1??????*" -exec du -kd0 {} \; | sed $'s/[[:blank:]]/,/;s/\/.*\///;s/[[:space:]]/-/g;s/\///g;/\,\./d' | sort -nr  >> /Volumes/HEIMDALL/LIBRARY/studio/servsizes/all_servers/projs/projdirs_$datetime.csv
+
+# # # # # # # # # # # #
+#   The Odin Chapter  #
+# # # # # # # # # # # #
+
+du -kd1 /ODIN | sed $'s/[[:blank:]]/,/;s/\/.*\///;s/[[:space:]]/-/g;s/\///g;/\,\./d' | sort -nr >> /Volumes/HEIMDALL/LIBRARY/studio/servsizes/all_servers/odin/odin_$datetime.csv
+#
+find /ODIN/_WORK/ -type d -and -iname "*_p1??????" -and -not -ipath "*_p1??????/*_p1??????*" -exec du -kd0 {} \; | sed 's/[[:blank:]]/,/g;s/\/.*\///' | sed '/\(\..*\)/d;s/\///g;s/[[:space:]]/-/g' | sort -nr >> /Volumes/HEIMDALL/LIBRARY/studio/servsizes/all_servers/work/workdirs_$datetime.csv
+#
+find /ODIN/LIBRARY/pFINALS/ -type d -and -iname "*_p1??????" -and -not -ipath "*_p1??????/*_p1??????*" -exec du -kd0 {} \; | sed 's/[[:blank:]]/,/g;s/\/.*\///' | sed '/\(\..*\)/d;s/\///g;s/[[:space:]]/-/g' | sort -nr >> /Volumes/HEIMDALL/LIBRARY/studio/servsizes/all_servers/fins/pfins_$datetime.csv
+
+# # # # # # # # # # # #
+#     The Rushes      #
+# # # # # # # # # # # #
+
+du -kd1 /RUSHES | sed $'s/[[:blank:]]/,/;s/\/.*\///;s/[[:space:]]/-/g;s/\///g;/\,\./d' | sort -nr >> /Volumes/HEIMDALL/LIBRARY/studio/servsizes/all_servers/rushes/rushes_$datetime.csv
+
+# # # # # # # # # # # #
+#      Currents       #
+# # # # # # # # # # # #
+
+rm -f /Volumes/HEIMDALL/LIBRARY/studio/servsizes/LATEST/*.csv
+for latest in /Volumes/HEIMDALL/LIBRARY/studio/servsizes/all_servers/{work,rushes}/*_$datetime.csv; do
+  cp "$latest" /Volumes/HEIMDALL/LIBRARY/studio/servsizes/LATEST/
+done
