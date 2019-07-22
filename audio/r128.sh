58984167031d8f273641707850204755ada2e855
Jorn Bergmans <jorn@ambassadors.studio>
2019-07-22 11:01:21 +0200

    Fixed find command to exclude dotfiles. Placed IFS higher up so output of find does not words split on whitespace characters.

--- audio/r128.sh
+++ audio/r128.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
+IFS=$'\n'
 
 if [[ -f $1 ]]; then
-  flist="$1" ;
+  flist="$1";
 elif [[ -d $1 ]]; then
-  flist=$(find "$1" -type f -iname "*.wav" -or -iname "*.mxf" -or -iname "*.mov" -or -iname "*.mp4" -or -iname "*.mkv" -or -iname "*.avi" -or -iname "*.flv" -or -iname "*.webm" -or -iname "*.aif*")
+  flist=$(find $1 -type f -not -iname ".*" -and -iname "*.mov" -or -iname "*.wav" -or -iname "*.aif*" -or -iname "*.mp4" -or -iname "*.mxf" -or -iname "*.webm" -or -iname "*.wmv" -or -iname "*.mkv" -or -iname "*.avi" -or -iname "*.flv")
 fi
 
 for f in $flist ; do
   if [[ -f $f ]]; then
-    IFS=$'\n' basename "$f" && ffprobe -f lavfi amovie="$f",ebur128=metadata=1 -show_frames 2>&1 | grep "I:";
+    echo "Probing loudness of \"$(basename $f)\""
+    ffprobe -f lavfi amovie="$f",ebur128=metadata=1 -show_frames 2>&1 | grep "I:";
   fi;
 done;
